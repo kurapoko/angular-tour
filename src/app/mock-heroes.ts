11a6e09e9d5cd6b995ba01ec72573b1cb31318f9
m.kurahara <kuraharamasahiro@gmail.com>
2020-10-01 00:44:48 +0900

    chapter1 to chapter4

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: '仮面ライダー' },
+  { id: 12, name: '孫悟空' },
+  { id: 13, name: 'スーパーマン' },
+  { id: 14, name: 'ミスターサタン' },
+];
