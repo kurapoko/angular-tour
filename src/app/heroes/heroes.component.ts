0a278b1c1a5a7b8c8222efc4f0a77a3cc715bc9c
m.kurahara <kuraharamasahiro@gmail.com>
2020-10-14 06:40:00 +0900

    chapter5

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService} from '../message.service'
 
 @Component({
   selector: 'app-heroes',
@@ -11,30 +10,16 @@ import { MessageService} from '../message.service'
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-    ) { }
+  constructor( private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    // this.heroes = this.heroService.getHeroes();
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: selected hero id=${hero.id}`);
-  }
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -5,4 +5,10 @@ export const HEROES: Hero[] = [
   { id: 12, name: '孫悟空' },
   { id: 13, name: 'スーパーマン' },
   { id: 14, name: 'ミスターサタン' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
 ];
